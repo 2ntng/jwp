be7fede97422e4928841cf6541074489820b898d
1917051045 <muhammadbintang2727@gmail.com>
2023-08-09 14:49:58 +0700

    Big O Faktorial

---
+++ public/hasil.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Hasil</title>
+</head>
+<body>
+  
+<?php
+    $number = $_POST['a1'];
+
+    // Operation : 1 + (1 * n) + 1 = n + 2
+    // Big O : O(n)
+    function cara1($n){
+      $sum = 0;   //  1
+      for ($i=$n; $i > 0; $i--) {  // *n
+        $sum += $i;   // 1
+      }
+      return $sum; // 1
+    }
+
+    // Operation : 3
+    // Big O : O(1)
+    function cara3($n){
+      return ($n + 1) * $n / 2; // 4
+    }
+  
+  ?>
+  <table>
+    <tr>
+      <td>Cara 1</td>
+      <td>
+        <?php $starttime = microtime(true); ?>
+        : <?= cara1($_POST['a1']) ?>
+      </td>
+    </tr>
+    <tr>
+      <td>Execution time</td>
+      <td>: <?= microtime(true) - $starttime; ?></td>
+    </tr>
+    <tr></tr>
+    <tr>
+      <td>Cara 3</td>
+      <td>
+        <?php $starttime = microtime(true); ?>
+        : <?= cara3($_POST['a1']) ?>
+      </td>
+    </tr>
+    <tr>
+      <td>Execution time</td>
+      <td>: <?= microtime(true) - $starttime; ?></td>
+    </tr>
+  </table>
+
+
+  <br>
+  <a href="http://localhost/kompre"><button>Kembali</button></a>
+</body>
+</html>
